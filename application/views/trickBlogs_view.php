59c7f2b96c07a734f2a2e8a59c3d139865fe5d44
Kevin McCoy <kevinmccoy@earthlink.net>
2013-01-18 04:09:44 -0500

    Rename 2nd and 3rd trick ski file since they are not being used on the landing page
    On the Blog view for Trick ski, added more to the list to show 2nd and
    3rd blogs examples
    
    Foot Note: This haven't been done for the other pages - Slalom, Jump,
    Overall

--- application/views/trickBlogs_view.php
+++ application/views/trickBlogs_view.php
@@ -12,6 +12,18 @@
 		<h3>Trick Ski</h3>
 		<p>Trick skiing is on one much smaller ski and the footplates are set differently than a slalom ski.  The skier has a time limit to perform tricks that are worth points.  The boat speed varies on the rider’s preference; usually any were from 12 to 24 MPHs.</p>
 	</li>
+	<li class="trick">
+		<!-- <div id="invoice">Image Slice 1</div> -->
+		<img src="<?= base_url('inc/img/trick002.jpg') ?>" alt="Trick Skiier just before dusk" />
+		<h3>Trick Ski</h3>
+		<p>Trick skiing is on one much smaller ski and the footplates are set differently than a slalom ski.  The skier has a time limit to perform tricks that are worth points.  The boat speed varies on the rider’s preference; usually any were from 12 to 24 MPHs.</p>
+	</li>
+	<li class="trick">
+		<!-- <div id="invoice">Image Slice 1</div> -->
+		<img src="<?= base_url('inc/img/trick003.jpg') ?>" alt="Trick Skiier just before dusk" />
+		<h3>Trick Ski</h3>
+		<p>Trick skiing is on one much smaller ski and the footplates are set differently than a slalom ski.  The skier has a time limit to perform tricks that are worth points.  The boat speed varies on the rider’s preference; usually any were from 12 to 24 MPHs.</p>
+	</li>
 </ul><!--[END]#siteInfo-->
 
 <a class="landingBtn" href="<?= base_url('register') ?>">Click to be part of the Water Ski Community</a>
