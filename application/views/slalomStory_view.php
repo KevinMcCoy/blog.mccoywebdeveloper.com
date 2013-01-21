9d2a1f8a26a52fd5e96d81203ce46f821debea54
Kevin McCoy <kevinmccoy@earthlink.net>
2013-01-21 18:35:47 -0500

    Getting full story page started

---
+++ application/views/slalomStory_view.php
@@ -0,0 +1,20 @@
+<!-- Created on Jan 21, 2013 by Kevin McCoy -->
+<!-- [Start] views/slalomStory_view.php -->
+<div id="center">
+	<h3>Slalom</h3>
+</div>
+
+<ul id="slalomBlog">
+	<!-- TODO::  Change class and ID names here as well in the the java script file soon to be added. -->
+	<li class="slalom">
+		<!-- <div id="invoice">Image Slice 1</div> -->
+		<img src="<?= base_url('inc/img/slalom_landing_full.png') ?>" alt="Slalom Skiier just before dusk" />
+		<h5>Slalom Ski:</h5>
+		<p class="inline">Slalom skiing is on only one ski with two footplates.  Once the skier can ski crossing the boat's wake, the skier can then start going around six buoys in a ski course. The boat’s top speed is 36 MPHs.</p>
+	</li>
+</ul><!--[END]#slalomBlog-->
+
+<a class="landingBTN" href="<?= base_url('register') ?>">Click to be part of the Water Ski Community</a>
+
+<!-- [End] of file views/landing_view.php -->
+<!-- Location: .../application/views/slalomStory_view.php -->
