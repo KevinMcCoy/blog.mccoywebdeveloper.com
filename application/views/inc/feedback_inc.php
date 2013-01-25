be36fd485ed9489e41594fe720bc06aaf39ee651
Kevin McCoy <kevinmccoy@earthlink.net>
2013-01-25 15:57:14 -0500

    Spelling and grammar corrections

--- application/views/inc/feedback_inc.php
+++ application/views/inc/feedback_inc.php
@@ -8,37 +8,33 @@
 
 <div id="user_testing">
 
-	<p>Hello, this site is in alpha stage.  Alpha stage is where we start usability testing.  That means, we need you to tell us what you think about this site.  What part of this page of the site did you dislike or like and/or didn't understand.  Are there anything you think is missing or would like to have on this page above the line?</p>
+	<p>Hello this site is in alpha stage.  Alpha stage is where we start usability testing.  That means we need you to tell us what your feedback for this web page.  Is there anything on this page you dislike, like, and/or don'tunderstand?  Is there anything you think is missing or would like to add?</p>
 	<p>This testing period will last from Jan 16, 2012 to February 16, 2013</p>
 
 	<form id="userfeedback" action="feedback/submit" method="POST" autocomplete="off">
 		<fieldset>
-			<legend>User Feed Back Questions</legend>
+			<legend>User Feedback Questions</legend>
 			<ol>
 				<li>
-					<label for="fb_question_1">What you think of the Navagation bar at the top of the page?</label>
+					<label for="fb_question_1">Can you easily read and use the Navigation at the top right of the page? Any suggestions?</label>
 					<textarea id="fb_question_1" name="fb_question_1"></textarea>
 				</li>
 				<li>
-					<label for="fb_question_2">The contents that are displayed, after clicking on edit there a form, what you think of these two things?</label>
-					<textarea id="fb_question_2" name="fb_question_2"></textarea>
-				</li>
-				<li>
-					<label for="fb_question_3">What you think of the buttons, and placement of the buttons, any other buttons you would like to have?</label>
+					<label for="fb_question_2">What do you think of the layout and or buttons; the placement, color, and readability?</label>
 					<textarea id="fb_question_3" name="fb_question_3"></textarea>
 				</li>
 				<li>
-					<label for="fb_question_4">What you think of the animation/changes as you use this page?</label>
+					<label for="fb_question_3">What do you think of the animation/changes as you use this page?</label>
 					<textarea id="fb_question_4" name="fb_question_4"></textarea>
 				</li>
 				<li>
-					<label for="fb_question_5">Take a step back, thinking about the overall feel of this page, colors, layout, placement, and from the feel of the site what dose it reminds you of ?</label>
+					<label for="fb_question_4">Take a step back, and think about the overall feel of this page; the colors, layout, placement. Dose it remindyou of anything and or how do you feal?</label>
 					<textarea id="fb_question_5" name="fb_question_5"></textarea>
 				</li>
 			</ol>
 		</fieldset>
 		<fieldset>
-			<legend>Would you like to be contacted when the site gose live?</legend>
+			<legend>Would you like to be contacted when the site goes live?</legend>
 				<ol id="feedback_touchbase">
 					<li>
 						<label for="feedback_name">Name</label>
@@ -56,6 +52,6 @@
 		</fieldset>
 	</form>
 
-	<p id="thankyou">Thankyou for your time checking out our site.  Your usability testing and feedback will be greatly appreciated.</p>
+	<p id="thankyou">Thank you, your usability testing and feedback will be greatly appreciated.</p>
 
 </div><!-- [END] #user_testing -->
