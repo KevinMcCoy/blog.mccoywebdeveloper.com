4d67cb2543fbf5a5bb6f9e5c8b7929aa8a42f7cd
Kevin McCoy <kevinmccoy@earthlink.net>
2013-01-28 00:50:55 -0500

    Change h# tag to proper numbers

--- application/views/landing_view.php
+++ application/views/landing_view.php
@@ -13,28 +13,28 @@
 	<ul id="siteInfo">
 		<li class="slalom">
 			<a href="<?= base_url('index.php/Views/slalomMain') ?>">
-				<h4>Slalom Ski</h4>
+				<h2>Slalom Ski</h2>
 				<img src="<?= base_url('inc/img/slalom_landing.png') ?>" alt="Slalom Skiier just before dusk" width="200" height="200" />
 				<p>Goal to go around six buoys in a ski course. The boat’s top speed is 36 MPHs</p>
 			</a>
 		</li>
 		<li class="trick">
 			<a href="<?= base_url('index.php/Views/wip') ?>">
-				<h4>Trick Ski</h4>
+				<h2>Trick Ski</h2>
 				<img src="<?= base_url('inc/img/trick_landing.jpg') ?>" alt="Trick Skiier doing a flip" width="200" height="200" />
 				<p>The skier has a thirty seconds limit to perform tricks that are worth points</p>
 			</a>
 		</li>
 		<li class="jump">
 			<a href="<?= base_url('index.php/Views/wip') ?>">
-				<h4>Jump Ski</h4>
+				<h2>Jump Ski</h2>
 				<img src="<?= base_url('inc/img/jump_landing.png') ?>" alt="Jump Skiier flying in the sky" width="200" height="200" />
 				<p>Skier goes over a five to six feet fiberglass ramp. The boat’s top speed is 45MPHs</p>
 			</a>
 		</li>
 		<li class="overall">
 			<a href="<?= base_url('index.php/Views/wip') ?>">
-				<h4>Overall 3-Event</h4>
+				<h2>Overall 3-Event</h2>
 				<img src="<?= base_url('inc/img/overall_landing.png') ?>" alt="Overall Winners" width="200" height="200" />
 				<p>Adds slalom, trick, and jump scores together and determine the overall winner</p>
 			</a>
