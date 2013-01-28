02fea07a4c56b4479fbd54bf64a02c75200a0d26
Kevin McCoy <kevinmccoy@earthlink.net>
2013-01-28 04:23:53 -0500

    Added continue reading story

--- application/views/slalomBlogs_view.php
+++ application/views/slalomBlogs_view.php
@@ -6,44 +6,48 @@
 	<h3>Slalom</h3>
 </div>
 
-<ul class="blogsShort">
-	<li>
-		<a href="<?= base_url('index.php/Views/slalomStory') ?>">
-			<img src="<?= base_url('inc/img/slalom_landing.png') ?>" alt="Slalom Skiier just before dusk" />
-			<h3>Slalom Ski Title One</h3>
-		</a>
-		<div class="social">
+	<div class='left'>
+	<ul class="blogsShort">
+		<li>
+			<a href="<?= base_url('index.php/Views/slalomStory') ?>">
+				<img src="<?= base_url('inc/img/slalom_landing.png') ?>" alt="Slalom Skiier just before dusk" />
+				<h3>Slalom Ski Title One</h3>
+			</a>
+			<div class="social">
+				<h4>Kevin McCoy 01/02/2013</h4>
+				<!-- Google +1 button -->
+				<div class="googleplus">
+					<div class="g-plusone" data-size="medium" data-annotation="inline" data-width="200"></div>
+				</div>
+				<!-- Facebook like button -->
+				<div class="facebook">
+					<div class="fb-like" data-send="false" data-layout="button_count" data-width="100" data-show-faces="true"></div>
+				</div>
+			</div>
+			<p>Slalom skiing is on only one ski with two footplates.  Once the skier can ski crossing the boat's wake, the skier can then start going around six buoys in a ski course. The boat’s top speed is 36 MPHs.</p>
+			<a class="blog" href="<?= base_url('index.php/Views/slalomStory') ?>"><p>Continue reading story...</p></a>
+		</li>
+		<li><hr class="style_two" /></li>
+		<li>
+			<a href="<?= base_url('index.php/Views/slalomStory') ?>">
+				<img src="<?= base_url('inc/img/slalom_landing.png') ?>" alt="Slalom Skiier just before dusk" />
+				<h3>Slalom Ski Title Two</h3>
+			</a>
 			<h4>Kevin McCoy 01/02/2013</h4>
-			<!-- Google +1 button -->
-			<div class="googleplus">
-				<div class="g-plusone" data-size="medium" data-annotation="inline" data-width="200"></div>
-			</div>
-			<!-- Facebook like button -->
-			<div class="facebook">
-				<div class="fb-like" data-send="false" data-layout="button_count" data-width="100" data-show-faces="true"></div>
-			</div>
-		</div>
-		<p>Slalom skiing is on only one ski with two footplates.  Once the skier can ski crossing the boat's wake, the skier can then start going around six buoys in a ski course. The boat’s top speed is 36 MPHs.</p>
-		<a href="<?= base_url('index.php/Views/slalomStory') ?>"><p>Test Button</p></a>
-	</li>
-	<li><hr class="style_two" /></li>
-	<li>
-		<a href="<?= base_url('index.php/Views/slalomStory') ?>">
+			<p>Slalom skiing is on only one ski with two footplates.  Once the skier can ski crossing the boat's wake, the skier can then start going around six buoys in a ski course. The boat’s top speed is 36 MPHs.</p>
+		</li>
+		<li><hr class="style_two" /></li>
+		<li>
+			<!-- <div id="invoice">Image Slice 1</div> -->
 			<img src="<?= base_url('inc/img/slalom_landing.png') ?>" alt="Slalom Skiier just before dusk" />
-			<h3>Slalom Ski Title Two</h3>
-		</a>
-		<h4>Kevin McCoy 01/02/2013</h4>
-		<p>Slalom skiing is on only one ski with two footplates.  Once the skier can ski crossing the boat's wake, the skier can then start going around six buoys in a ski course. The boat’s top speed is 36 MPHs.</p>
-	</li>
-	<li><hr class="style_two" /></li>
-	<li>
-		<!-- <div id="invoice">Image Slice 1</div> -->
-		<img src="<?= base_url('inc/img/slalom_landing.png') ?>" alt="Slalom Skiier just before dusk" />
-		<h3>Slalom Ski Title Three</h3>
-		<h4>Kevin McCoy 01/02/2013</h4>
-		<p>Slalom skiing is on only one ski with two footplates.  Once the skier can ski crossing the boat's wake, the skier can then start going around six buoys in a ski course. The boat’s top speed is 36 MPHs.</p>
-	</li>
-</ul><!--[END]#slalomBlog-->
+			<h3>Slalom Ski Title Three</h3>
+			<h4>Kevin McCoy 01/02/2013</h4>
+			<p>Slalom skiing is on only one ski with two footplates.  Once the skier can ski crossing the boat's wake, the skier can then start going around six buoys in a ski course. The boat’s top speed is 36 MPHs.</p>
+		</li>
+	</ul><!--[END]#slalomBlog-->
+</div>
+<div class="right"></div>
+
 <div class="clear"></div>
 <!-- [End] of file views/landing_view.php -->
 <!-- Location: .../application/views/landing_views.php -->
