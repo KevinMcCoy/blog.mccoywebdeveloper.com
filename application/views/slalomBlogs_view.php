768e8d6463eb9bd15ac88d0ef8803b2691be7970
Kevin McCoy <kevinmccoy@earthlink.net>
2013-01-28 03:10:21 -0500

    WIP Testing Google and Facebook API aligin to the middle
    WIP work in progress

--- application/views/slalomBlogs_view.php
+++ application/views/slalomBlogs_view.php
@@ -1,5 +1,7 @@
 <!-- Created on Jan 15, 2013 by Kevin McCoy -->
 <!-- [Start] views/landing_view.php -->
+
+<!-- Hidden This is were you can add a center in another development stage IDEA:: -->
 <div id="center" style="display:none; visibility:hidden;">
 	<h3>Slalom</h3>
 </div>
@@ -10,15 +12,21 @@
 			<img src="<?= base_url('inc/img/slalom_landing.png') ?>" alt="Slalom Skiier just before dusk" />
 			<h3>Slalom Ski Title One</h3>
 		</a>
+<div class="social">
+
 		<h4>Kevin McCoy 01/02/2013</h4>
-		<p class="inline">Slalom skiing is on only one ski with two footplates.  Once the skier can ski crossing the boat's wake, the skier can then start going around six buoys in a ski course. The boat’s top speed is 36 MPHs.</p>
-		<span class="socialmedia">
-			<!-- Google +1 button --><div class="g g-plusone" data-size="tall" data-annotation="none"></div>
+
+		<span class="googleplus">
+			<!-- Google +1 button --><div class="g-plusone" size="small" data-annotation="inline" data-width="200"></div>
 		</span>
-		<span class="socialmedia">
-			<!-- Facebook like button --><div class="fb-like" data-send="true" data-layout="button_count" data-width="100" data-show-faces="true" float="right"></div>
+
+		<span class="facebook">
+			<!-- Facebook like button --><div class="fb-like" data-send="true" data-layout="button_count" data-width="100" data-show-faces="true"></div>
 		</span>
+</div>
+		<p>Slalom skiing is on only one ski with two footplates.  Once the skier can ski crossing the boat's wake, the skier can then start going around six buoys in a ski course. The boat’s top speed is 36 MPHs.</p>
 	</li>
+
 	<hr class="style_two" />
 	<li>
 		<a href="<?= base_url('index.php/Views/slalomStory') ?>">
@@ -27,12 +35,6 @@
 		</a>
 		<h4>Kevin McCoy 01/02/2013</h4>
 		<p>Slalom skiing is on only one ski with two footplates.  Once the skier can ski crossing the boat's wake, the skier can then start going around six buoys in a ski course. The boat’s top speed is 36 MPHs.</p>
-		<span class="socialmedia">
-			<!-- Google +1 button --><div class="g g-plusone" data-size="tall" data-annotation="none"></div>
-		</span>
-		<span class="socialmedia">
-			<!-- Facebook like button --><div class="fb-like" data-send="true" data-layout="button_count" data-width="100" data-show-faces="true" float="right"></div>
-		</span>
 	</li>
 	<hr class="style_two" />
 	<li>
@@ -41,16 +43,6 @@
 		<h3>Slalom Ski Title Three</h3>
 		<h4>Kevin McCoy 01/02/2013</h4>
 		<p>Slalom skiing is on only one ski with two footplates.  Once the skier can ski crossing the boat's wake, the skier can then start going around six buoys in a ski course. The boat’s top speed is 36 MPHs.</p>
-
-<!-- Place this tag where you want the +1 button to render. -->
-<div class="g-plusone" data-annotation="inline" data-width="300"></div>
-
-
-			<!-- Google +1 button --><div class="g g-plusone" data-size="tall" data-annotation="none"></div>
-
-		<span class="socialmedia">
-			<!-- Facebook like button --><div class="fb-like" data-send="true" data-layout="button_count" data-width="100" data-show-faces="true" float="right"></div>
-		</span>
 	</li>
 </ul><!--[END]#slalomBlog-->
 
