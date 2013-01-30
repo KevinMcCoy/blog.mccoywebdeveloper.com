263f67ca6ef3e3340e8e9c48d88e0e6194e95381
Kevin McCoy <kevinmccoy@earthlink.net>
2013-01-29 19:40:23 -0500

    Commits added, uncommits some codes

--- application/views/slalomBlogs_view.php
+++ application/views/slalomBlogs_view.php
@@ -8,21 +8,22 @@
 
 	<div class='left'>
 	<ul class="blogsShort">
+		<!-- itemscope itemtype is for google + button to grab the data using jQuery - Validation error says: Line 106, Column 55: Bad value www.McCoyWebDeveloper.com/ for attribute itemtype on element li: Not an absolute IRI. -->
 		<li itemscope itemtype="www.McCoyWebDeveloper.com/" >
 			<a href="<?= base_url('index.php/Views/slalomStory') ?>">
 				<img itemprop="image" src="<?= base_url('inc/img/slalom_landing.png') ?>" width="200" height="160" alt="Slalom Skiier just before dusk" />
 				<h3 itemprop="name">Slalom Ski Title One</h3>
 			</a>
-			<div class="social" style="visibility: collapse;">
+			<div class="social">
 				<h4 class="tempTest">Kevin McCoy 01/02/2013</h4>
 				<!-- Facebook like button -->
 				<div class="facebook">
-					<!-- <div class="fb-like" data-send="false" data-layout="button_count" data-width="100" data-show-faces="true"></div> -->
+					<div class="fb-like" data-send="false" data-layout="button_count" data-width="100" data-show-faces="true"></div>
 				</div>
 				<!-- Google +1 button -->
 				<div class="googleplus">
 					<!-- <div class="g-plus" data-action="share"></div> -->
- 					<!-- <div class="g-plusone" data-size="medium" data-annotation="inline" data-width="200"></div> -->
+ 					<div class="g-plusone" data-size="medium" data-annotation="inline" data-width="200"></div>
  				</div>
 			</div>
 			<p itemprop="description">Slalom skiing is on only one ski with two footplates.  Once the skier can ski crossing the boat's wake, the skier can then start going around six buoys in a ski course. The boat’s top speed is 36 MPHs.</p>
