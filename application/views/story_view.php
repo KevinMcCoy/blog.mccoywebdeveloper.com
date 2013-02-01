f9d58937066aa2cb0a501fdf813fe12484f94022
Kevin McCoy <kevinmccoy@earthlink.net>
2013-02-01 07:09:05 -0500

    Story page, line social buttons to the left side

--- application/views/story_view.php
+++ application/views/story_view.php
@@ -10,10 +10,9 @@
 <div class='left'>
 	<ul id="story">
 		<!-- TODO::  Change class and ID names here as well in the the java script file soon to be added. -->
-		<li itemscope itemtype="www.McCoyWebDeveloper.com/" >
+		<li itemscope itemtype="www.McCoyWebDeveloper.com/" ></li>
 			<h3><?= $dbBlogTitle[0] ?></h3>
-			<a class="text" href="<?= base_url('index.php/Views/'.$current.'Blogs') ?>">Back to <?= $current ?> Blogs list</a>
-			<div class="social wide">
+			<div class="social">
 				<h4><?= $dbWriter[0].' '.$dbWriterDate[0] ?></h4>
 				<!-- Facebook like button -->
 				<div class="facebook">
@@ -23,6 +22,7 @@
 				<div class="googleplus">
 					<div class="g-plusone" data-size="medium" data-annotation="inline" data-width="200"></div>
 				</div>
+			<a class="text" href="<?= base_url('index.php/Views/'.$current.'Blogs') ?>">Back to <?= $current ?> Blogs list</a>
 			</div>
 			<img src="<?= base_url('inc/img/story/slalom_001.jpg')?>" width="730" height="560" alt="Skiier just before dusk" />
 			<p itemprop="description"><?= $dbStoryLong[0] ?></p>
