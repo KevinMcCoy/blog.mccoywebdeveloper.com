afa8be89f2e493b27521f62b3dbaeea50a7cbe7b
Kevin McCoy <kevinmccoy@earthlink.net>
2013-02-01 08:46:02 -0500

    About page images added, page styled

--- application/views/about_view.php
+++ application/views/about_view.php
@@ -1,30 +1,33 @@
 
-<h1>About</h1>
-
+<h2>Read About 3 Event Water Skiing</h2>
 <!-- Details about Info listed from the four li above -->
 <ul id="about3Event">
 	<li>
-		<img src="<?= base_url('inc/img/about/slalom_001.jpg') ?>" alt="Slalom Skiier just before dusk" />
-		<h3>Slalom</h3>
+		<img src="<?= base_url('inc/img/about/slalom.jpg') ?>" alt="Slalom Skiier just before dusk" />
+		<h3>Slalom Ski</h3>
 		<p>Slalom skiing is on only one ski with two footplates.  Once the skier can ski crossing the boat's wake, the skier can then start going around six buoys in a ski course. The boat’s top speed is 36mph.</p>
-		<a class="continueReadingStory" href="<?= base_url('index.php/Views/slalomBlogs') ?>">Check out the Slalom blog</a>
+		<a class="" href="<?= base_url('index.php/Views/slalomBlogs') ?>">View Our Slalom Ski blogs page</a>
 	</li>
 	
 	<li>
-		<img src="<?= base_url('inc/img/trick_landing.jpg') ?>" alt="Slalom Skiier just before dusk" />
-		<h3>Trick</h3>
+		<img src="<?= base_url('inc/img/about/trick.jpg') ?>" alt="Slalom Skiier just before dusk" />
+		<h3>Trick Ski</h3>
 		<p>Trick skiing is on one or two much smaller ski/s and the footplates are set differently than a slalom ski.  The skier has a time limit to perform tricks that are worth points.  The boat speed varies on the rider’s preference; usually anywhere from 12mph to 24mph.</p>
+		<a class="" href="<?= base_url('index.php/Views/trickBlogs') ?>">View Our Trick Ski blogs page</a>
 	</li>
 	
 	<li>
-		<img src="<?= base_url('inc/img/jump_landing.png') ?>" alt="Slalom Skiier just before dusk" />
-		<h3>Jump</h3>
+		<img src="<?= base_url('inc/img/about/jump.jpg') ?>" alt="Slalom Skiier just before dusk" />
+		<h3>Jump Ski</h3>
 		<p>Jump skiing is when the skier skis on two skis. They are larger, longer, and wider than a slalom ski.  The skier swings from one side to the other, crossing the boat’s wake. A pro-skier can travel to a speed up to 90mph just before hitting a five or six feet fiberglass ramp. It is hard to believe that the boat’s top speed is 45mph. The world record holder in jump skiing flew over 300 feet in the air.</p>
+		<a class="" href="<?= base_url('index.php/Views/jumpBlogs') ?>">View Our Jump Ski blogs page</a>
 	</li>
 
 	<li>
-		<img src="<?= base_url('inc/img/overall_landing.png') ?>" alt="Slalom Skiier just before dusk" />
-		<h3>Overall</h3>
+		<img src="<?= base_url('inc/img/about/overall.jpg') ?>" alt="Slalom Skiier just before dusk" />
+		<h3>Overall Scoring</h3>
 		<p>The meaning of the term Overall is a scoring system.  The way overall scoring works is to adds the slalom, trick, and jump scores together.  The combined total score will determine the winner of the Overall 3-Event.  Overall scores are figured out at every official tournament that qualifies a contender for all three events.</p>
+		<a class="" href="<?= base_url('index.php/Views/overallBlogs') ?>">View Our Overall Score blogs page</a>
 	</li>
-</ul><!--[END] #siteInfoDetails
+</ul><!--[END] #siteInfoDetails -->
+<div class="clear"></div>
