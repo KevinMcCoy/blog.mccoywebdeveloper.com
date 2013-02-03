53ca557f03855bbae245b42b588e5334a4ff0070
Kevin McCoy <kevinmccoy@earthlink.net>
2013-02-02 19:33:41 -0500

    Spelling Fix

--- application/views/inc/intro_01_inc.php
+++ application/views/inc/intro_01_inc.php
@@ -7,7 +7,7 @@
 	<!-- <blockquote> --><!-- <span class="quotes left">&ldquo;</span> -->
 	<h3>Welcome to the water ski community!</h3>
 	<h3>Read what others are talking about...</h3>
-	<p>Here you will find interesting reads in the four topic base on Three Event Water Skiing.  If you never heard of this sport, you can read here exactly about what Three Event Water Skiing is all about.</p><!-- <span class="quotes right">&rdquo;</span> --><!-- </blockquote> -->
+	<p>Here you will find interesting reads in the four topic based on Three Event Water Skiing. If you never heard of this sport, read here exactly what Three Event Water Skiing is all about.</p><!-- <span class="quotes right">&rdquo;</span> --><!-- </blockquote> -->
 	<input type="button" name="aboutThreeEvent" onClick="location.href='<?= base_url('index.php/Views/about3Event') ?>'" value="Read About 3 Event Water Skiing">
 	<!-- a href - Learn about 3-Event -->
 </div><!-- [END] #shortIntro -->
